a56d9a6862c68532f126e284d12e40cc86f335fd
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-09-23 19:29:46 +0530

    lac-13

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,28 +1,43 @@
 import { useState } from 'react';
 import './App.css';
+import Alert from './components/Alert';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
   const[mode, setMode]=useState('light');//  --->darkmode is unable or not
+  const[alert,setalert]=useState('null')
+
+  function showalert(message,typee){
+    setalert({
+      msg:message,
+      type:typee
+    })
+    setTimeout(() => {
+      setalert(null)
+    }, 1500);
+  }
 
   const togglemode=()=>{
     if(mode === "light"){
       setMode('dark')
       document.body.style.backgroundColor="grey"
+      showalert("dark mode has been enabled","success")
     }
     else{
       setMode('light')
       document.body.style.backgroundColor="white"
+      showalert("light mode has been enabled","success")
     }
   }
 
   return (
     <>
       <Navbar title="textutils" abouttext="about us" modee={mode} togglemodeee={togglemode}/>
+      <Alert alert={alert}/>
       <div className="container">
-        <TextForm heading="Enter the heading" modee={mode}/>
+        <TextForm showalerttt={showalert} heading="Enter the heading" modee={mode}/>
         {/* <About /> */}
       </div>
     </>
--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -7,50 +7,57 @@ const handleupclick =()=>{
         let newText=Text.toUpperCase();
         setText(newText);
         // setText('you have cliced on handleupclick')  
+        props.showalerttt("Converted to UpperCase","success")
     }
 
     const handleuplower =()=>{
         let newText=Text.toLowerCase();
         setText(newText)
+        props.showalerttt("Converted to LowerCase","success")
     }
 
     const handleupclear =()=>{
         let newText="";
         setText(newText)
+        props.showalerttt("Clear the text","success")
     }
 
-    const handleupnew =()=>{
-        let newText=Text.charAt(0).toUpperCase() + Text.slice(" ");
+    const handelcapitalize =()=>{
+        let newText=Text.charAt(0).toUpperCase() + Text.slice(1)
         setText(newText)
+        props.showalerttt("Text converted to capitalize","success")
+    }
+    
+    const handlefirslatter =()=>{
+        let newText=Text.split(" ")
+      
     }
 
+    //Give permission to add text in textbarear
     const handleOnchange =(event)=>{
         console.log('on change');
         setText(event.target.value)
     }
-    const handlefirslatter =()=>{
-        let newtext=Text.split(" ").map() 
-        
-    }
 
-    const handlecapital=()=>{
-        let newtext=Text.charAt(0).toUpperCase()
-        setText(newtext)
-    }
+
+   
 
     const handlecopy=()=>{
         let Text=document.getElementById('mybox');
         Text.select()
         navigator.clipboard.writeText(Text.value)
+        props.showalerttt("Text copy to cliboard","success")
     }
 
     const handelExtraSpace=()=>{
         let newtext=Text.split(/[ ]+/);
         setText(newtext.join(' '))
+        props.showalerttt("remove extra spaces","success")
     }
 
 
     const [Text, setText] = useState("enter the text");
+    // setText("your text")
     // Text="dfdhd"       //u can not set text this way....in react  /wrong eay to change the state
     // setText("ready to set");        //correct way
     return (
@@ -61,9 +68,8 @@ const handleupclick =()=>{
             <button className='btn btn-success my-2' onClick={handleupclick}>Convert to upper case</button>
             <button className='btn btn-outline-danger mx-3 my-2' onClick={handleuplower}>Convert to lower case</button>
             <button className='btn btn-warning my-2' onClick={handleupclear}>Clear text </button>
-            <button className='btn btn-primary mx-3 my-2' onClick={handlecapital}>Captlize</button>
+            <button className='btn btn-primary mx-3 my-2' onClick={handelcapitalize}>Captlize</button>
             <button className='btn btn-primary my-2' onClick={handlefirslatter}>first latter Cap.</button>
-            {/* <button className='btn btn-primary mx-3 my-2' onClick={handleallcap}>All latter Cap.</button> */}
             <button className='btn btn-primary mx-3 my-2' onClick={handlecopy}>copy text</button>
             <button className='btn btn-primary my-2' onClick={handelExtraSpace}>remove space</button>
         </div>
