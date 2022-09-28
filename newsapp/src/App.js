b323046d02628f6acdd0e3555a502d7b80a5567d
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-09-28 21:43:49 +0530

    lac-22 newsapp

---
+++ newsapp/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  c="joy"
+  render() {
+    return (
+      <div>
+        hello class base commponent {this.c}
+        
+      </div>
+    )
+  }
+}
