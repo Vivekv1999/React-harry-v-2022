aec4a17e1cc4c4a6569000d078af02756ba8ab0c
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-09-30 17:50:25 +0530

    lac-27 news-though-fetch api...

--- newsapp/src/Component/NewsItem.js
+++ newsapp/src/Component/NewsItem.js
@@ -12,7 +12,7 @@ export default class NewsItem extends Component {
         <div className="card" style={{width: "18rem"}}>     {/*style="width: 18rem"  --> aam hatu pela ,,,,*/}
           <img src={imageUrl} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
+              <h5 className="card-title">{title}...</h5>   {/*//////////this.props.title    ////$$$///   aaam lay jovu*/}
               <p className="card-text">{description}...</p>
               <a href={newsUrl} target="_blank" className="btn btn-primary  btn-sm">Read More</a>
             </div>
