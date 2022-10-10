669440738f42bc3f1374ecfe72b07ba29f861a7c
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-10-10 21:45:53 +0530

    lac-46 dara validation using express...

--- inotebook/BACKEND/routes/auth.js
+++ inotebook/BACKEND/routes/auth.js
@@ -1,14 +1,33 @@
 const express = require('express')
 const router = express.Router();
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
 
 //create user using : POST "api/auth/". DOes not req. authetiction
-router.post('/', (req, res) => {
-    console.log(req.body);
-    const user = User(req.body)
-    user.save()
-    res.send(req.body);
+router.post('/', [
+    body('name').isLength({ min: 3 }),
+    body('email',"enter a valid email").isEmail(),
+    body('password').isLength({ min: 5 })
+],(req, res) => {
+
+const errors=validationResult(req)
+    if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+}
+
+User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  }).then(user => res.json(user))
+  .catch(err=>console.log(err) )
+
+
+// console.log(req.body);
+// const user = User(req.body)
+// user.save()
+// res.send(req.body);
 
 })
 
