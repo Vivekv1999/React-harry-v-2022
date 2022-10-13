25f60707c471cccfdc4ef5dafdc872de23820806
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-10-13 22:27:44 +0530

    lac-50 login end point & sending auth token

--- inotebook/BACKEND/routes/auth.js
+++ inotebook/BACKEND/routes/auth.js
@@ -68,5 +68,42 @@ router.post('/createuser', [
 
 })
 
+//authenticate a user uding : post"/api/auth/login"
+router.post('/login', [
+    body('email', "enter a valid email").isEmail(),
+    body('password', "password can not be empty").exists(),
+], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
+    const {email, password} = req.body
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ error: 'please try to login with correct credendtial' })
+
+        }
+        const passwordcompare = await bcrypt.compare(password, user.password);
+        if (!passwordcompare) {
+            return req.status(400).json({
+                error: "please try to login with correct credential"
+            })
+
+        }
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET)
+        res.send({ authtoken })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server Error occured")
+    }
+
+
+})
 module.exports = router
