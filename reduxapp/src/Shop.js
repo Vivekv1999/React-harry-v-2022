0312ea540d66312806db90a92370d332105cc5c0
Vivekv1999 <vivekvekariya1999@gmail.com>
2022-11-04 18:26:04 +0530

    lac-75 creating reducer and action creator in redux

--- reduxapp/src/Shop.js
+++ reduxapp/src/Shop.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Shop() {
   return (
     <div>
-        <h3 className='my-3'>Buy Adidas shoes 50 Rs</h3>
+        <h3 className='my-3'>Doposite/Withdra Money</h3>
       <button className='btn btn-primary mx-2'>-</button>
-      Add to card
+      Update balance
       <button className='btn btn-primary mx-2'>+</button>
     </div>
   )
---
+++ reduxapp/src/State/action-creator/index.js
@@ -0,0 +1,17 @@
+export const depositemoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: "deposite",
+            payload: amount
+        })
+    }
+}
+
+export const withdrawmoney = (amount) => {
+    return (dispatch) => {
+        dispatch({
+            type: "withdraw",
+            payload: amount
+        })
+    }
+}
